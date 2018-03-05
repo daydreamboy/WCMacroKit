04d1ab6541782083291ae14acf917d9525948ab4
daydreamboy <wesley4chen@gmail.com>
2018-03-05 17:30:27 +0800

    add code

--- WCMacroKit/WCMacroDelegate.h
+++ WCMacroKit/WCMacroDelegate.h
@@ -12,6 +12,8 @@
 /// Delegate caller
 #pragma mark Delegate caller
 
+#pragma mark > With return value
+
 #ifndef DELEGATE_SAFE_CALL_WITH_RETURN
 #define DELEGATE_SAFE_CALL_WITH_RETURN(delegate, self) \
     ({ \
@@ -123,6 +125,64 @@
     })
 #endif /* DELEGATE_SAFE_CALL4_WITH_RETURN */
 
+#ifndef DELEGATE_SAFE_CALL5_WITH_RETURN
+#define DELEGATE_SAFE_CALL5_WITH_RETURN(delegate, sel, arg1, arg2, arg3, arg4, arg5) \
+    ({ \
+        id returnValue = nil; \
+        if ([delegate respondsToSelector:sel]) { \
+            typeof(arg1) param1 = arg1; \
+            typeof(arg2) param2 = arg2; \
+            typeof(arg3) param3 = arg3; \
+            typeof(arg4) param4 = arg4; \
+            typeof(arg5) param5 = arg5; \
+            void *tempReturnValue = nil; \
+            NSMethodSignature *methodSignature = [delegate methodSignatureForSelector:sel]; \
+            NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:methodSignature]; \
+            invocation.target = delegate; \
+            invocation.selector = sel; \
+            [invocation setArgument:(void *)&param1 atIndex:2]; \
+            [invocation setArgument:(void *)&param2 atIndex:3]; \
+            [invocation setArgument:(void *)&param3 atIndex:4]; \
+            [invocation setArgument:(void *)&param4 atIndex:5]; \
+            [invocation setArgument:(void *)&param5 atIndex:6]; \
+            [invocation invoke]; \
+            [invocation getReturnValue:&tempReturnValue]; \
+            returnValue = (__bridge id)tempReturnValue; \
+        } \
+        returnValue; \
+    })
+#endif /* DELEGATE_SAFE_CALL5_WITH_RETURN */
+
+#ifndef DELEGATE_SAFE_CALL6_WITH_RETURN
+#define DELEGATE_SAFE_CALL6_WITH_RETURN(delegate, sel, arg1, arg2, arg3, arg4, arg5, arg6) \
+    ({ \
+        id returnValue = nil; \
+        if ([delegate respondsToSelector:sel]) { \
+            typeof(arg1) param1 = arg1; \
+            typeof(arg2) param2 = arg2; \
+            typeof(arg3) param3 = arg3; \
+            typeof(arg4) param4 = arg4; \
+            typeof(arg5) param5 = arg5; \
+            typeof(arg6) param6 = arg6; \
+            void *tempReturnValue = nil; \
+            NSMethodSignature *methodSignature = [delegate methodSignatureForSelector:sel]; \
+            NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:methodSignature]; \
+            invocation.target = delegate; \
+            invocation.selector = sel; \
+            [invocation setArgument:(void *)&param1 atIndex:2]; \
+            [invocation setArgument:(void *)&param2 atIndex:3]; \
+            [invocation setArgument:(void *)&param3 atIndex:4]; \
+            [invocation setArgument:(void *)&param4 atIndex:5]; \
+            [invocation setArgument:(void *)&param5 atIndex:6]; \
+            [invocation setArgument:(void *)&param6 atIndex:7]; \
+            [invocation invoke]; \
+            [invocation getReturnValue:&tempReturnValue]; \
+            returnValue = (__bridge id)tempReturnValue; \
+        } \
+        returnValue; \
+    })
+#endif /* DELEGATE_SAFE_CALL6_WITH_RETURN */
+
 #ifndef DELEGATE_SAFE_CALL7_WITH_RETURN
 #define DELEGATE_SAFE_CALL7_WITH_RETURN(delegate, sel, arg1, arg2, arg3, arg4, arg5, arg6, arg7) \
     ({ \
@@ -155,34 +215,39 @@
     })
 #endif /* DELEGATE_SAFE_CALL7_WITH_RETURN */
 
-/// @warning 1. Only apply for delegate methods without return value 2. Only support two arguments at most
+#pragma mark > No return value
 
-#ifndef DELEGATE_SAFE_PERFORM
-#define DELEGATE_SAFE_PERFORM(delegate, sel)        \
-    do {                                            \
-        if ([delegate respondsToSelector:sel]) {    \
-            [delegate performSelector:sel];         \
-        }                                           \
+#ifndef DELEGATE_SAFE_CALL1
+#define DELEGATE_SAFE_CALL1(delegate, sel, arg1) \
+    do { \
+        if ([delegate respondsToSelector:sel]) { \
+            typeof(arg1) param1 = arg1; \
+            NSMethodSignature *methodSignature = [delegate methodSignatureForSelector:sel]; \
+            NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:methodSignature]; \
+            invocation.target = delegate; \
+            invocation.selector = sel; \
+            [invocation setArgument:(void *)&param1 atIndex:2]; \
+            [invocation invoke]; \
+        } \
     } while (0)
-#endif /* DELEGATE_SAFE_PERFORM */
+#endif /* DELEGATE_SAFE_CALL1 */
 
-#ifndef DELEGATE_SAFE_PERFORM1
-#define DELEGATE_SAFE_PERFORM1(delegate, sel, arg1)         \
-    do {                                                    \
-        if ([delegate respondsToSelector:sel]) {            \
-            [delegate performSelector:sel withObject:arg1]; \
-        }                                                   \
+#ifndef DELEGATE_SAFE_CALL2
+#define DELEGATE_SAFE_CALL2(delegate, sel, arg1, arg2) \
+    do { \
+        if ([delegate respondsToSelector:sel]) { \
+            typeof(arg1) param1 = arg1; \
+            typeof(arg2) param2 = arg2; \
+            NSMethodSignature *methodSignature = [delegate methodSignatureForSelector:sel]; \
+            NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:methodSignature]; \
+            invocation.target = delegate; \
+            invocation.selector = sel; \
+            [invocation setArgument:(void *)&param1 atIndex:2]; \
+            [invocation setArgument:(void *)&param2 atIndex:3]; \
+            [invocation invoke]; \
+        } \
     } while (0)
-#endif /* DELEGATE_SAFE_PERFORM1 */
-
-#ifndef DELEGATE_SAFE_PERFORM2
-#define DELEGATE_SAFE_PERFORM2(delegate, sel, arg1, arg2)                   \
-    do {                                                                    \
-        if ([delegate respondsToSelector:sel]) {                            \
-            [delegate performSelector:sel withObject:arg1 withObject:arg2]; \
-        }                                                                   \
-    } while (0)
-#endif /* DELEGATE_SAFE_PERFORM2 */
+#endif /* DELEGATE_SAFE_CALL2 */
 
 #ifndef DELEGATE_SAFE_CALL3
 #define DELEGATE_SAFE_CALL3(delegate, sel, arg1, arg2, arg3) \
@@ -224,6 +289,54 @@
     } while (0)
 #endif /* DELEGATE_SAFE_CALL4 */
 
+#ifndef DELEGATE_SAFE_CALL5
+#define DELEGATE_SAFE_CALL5(delegate, sel, arg1, arg2, arg3, arg4, arg5) \
+    do { \
+        if ([delegate respondsToSelector:sel]) { \
+            typeof(arg1) param1 = arg1; \
+            typeof(arg2) param2 = arg2; \
+            typeof(arg3) param3 = arg3; \
+            typeof(arg4) param4 = arg4; \
+            typeof(arg5) param5 = arg5; \
+            NSMethodSignature *methodSignature = [delegate methodSignatureForSelector:sel]; \
+            NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:methodSignature]; \
+            invocation.target = delegate; \
+            invocation.selector = sel; \
+            [invocation setArgument:(void *)&param1 atIndex:2]; \
+            [invocation setArgument:(void *)&param2 atIndex:3]; \
+            [invocation setArgument:(void *)&param3 atIndex:4]; \
+            [invocation setArgument:(void *)&param4 atIndex:5]; \
+            [invocation setArgument:(void *)&param5 atIndex:6]; \
+            [invocation invoke]; \
+        } \
+    } while (0)
+#endif /* DELEGATE_SAFE_CALL5 */
+
+#ifndef DELEGATE_SAFE_CALL6
+#define DELEGATE_SAFE_CALL6(delegate, sel, arg1, arg2, arg3, arg4, arg5, arg6) \
+    do { \
+        if ([delegate respondsToSelector:sel]) { \
+            typeof(arg1) param1 = arg1; \
+            typeof(arg2) param2 = arg2; \
+            typeof(arg3) param3 = arg3; \
+            typeof(arg4) param4 = arg4; \
+            typeof(arg5) param5 = arg5; \
+            typeof(arg6) param6 = arg6; \
+            NSMethodSignature *methodSignature = [delegate methodSignatureForSelector:sel]; \
+            NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:methodSignature]; \
+            invocation.target = delegate; \
+            invocation.selector = sel; \
+            [invocation setArgument:(void *)&param1 atIndex:2]; \
+            [invocation setArgument:(void *)&param2 atIndex:3]; \
+            [invocation setArgument:(void *)&param3 atIndex:4]; \
+            [invocation setArgument:(void *)&param4 atIndex:5]; \
+            [invocation setArgument:(void *)&param5 atIndex:6]; \
+            [invocation setArgument:(void *)&param6 atIndex:7]; \
+            [invocation invoke]; \
+        } \
+    } while (0)
+#endif /* DELEGATE_SAFE_CALL6 */
+
 #ifndef DELEGATE_SAFE_CALL7
 #define DELEGATE_SAFE_CALL7(delegate, sel, arg1, arg2, arg3, arg4, arg5, arg6, arg7) \
     do { \
@@ -250,6 +363,37 @@
         } \
     } while (0)
 #endif /* DELEGATE_SAFE_CALL7 */
+
+#pragma mark - Delegate performSelector
+
+/// @warning 1. Only apply for delegate methods without return value 2. Only support two arguments at most
+
+#ifndef DELEGATE_SAFE_PERFORM
+#define DELEGATE_SAFE_PERFORM(delegate, sel)        \
+    do {                                            \
+        if ([delegate respondsToSelector:sel]) {    \
+            [delegate performSelector:sel];         \
+        }                                           \
+    } while (0)
+#endif /* DELEGATE_SAFE_PERFORM */
+
+#ifndef DELEGATE_SAFE_PERFORM1
+#define DELEGATE_SAFE_PERFORM1(delegate, sel, arg1)         \
+    do {                                                    \
+        if ([delegate respondsToSelector:sel]) {            \
+            [delegate performSelector:sel withObject:arg1]; \
+        }                                                   \
+    } while (0)
+#endif /* DELEGATE_SAFE_PERFORM1 */
+
+#ifndef DELEGATE_SAFE_PERFORM2
+#define DELEGATE_SAFE_PERFORM2(delegate, sel, arg1, arg2)                   \
+    do {                                                                    \
+        if ([delegate respondsToSelector:sel]) {                            \
+            [delegate performSelector:sel withObject:arg1 withObject:arg2]; \
+        }                                                                   \
+    } while (0)
+#endif /* DELEGATE_SAFE_PERFORM2 */
 
 #pragma mark - Dummy Protocol
 
