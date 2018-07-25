c08ec2fa4f6b1df35ea94ee92cb644e71a82cd1a
daydreamboy <wesley4chen@gmail.com>
2018-07-25 19:17:59 +0800

    fix prepare_command_build.sh

--- prepare_command_build.sh
+++ prepare_command_build.sh
@@ -8,39 +8,49 @@ TARGET_NAME="WCMacroKit"
 PROJECT_NAME="WCMacroKit.xcodeproj"
 CONFIGURATION="Release"
 
+ARCH_IPHONEOS="iphoneos"
+ARCH_IPHONESIMULATOR="iphonesimulator"
+ARCH_IPHONEUNIVERSAL="iphoneuniversal"
+
+IPHONEOS_OUTPUT="${PRODUCT_DIR}/$CONFIGURATION-$ARCH_IPHONEOS"
+IPHONEOSIMULATOR_OUTPUT="${PRODUCT_DIR}/$CONFIGURATION-$ARCH_IPHONESIMULATOR"
+IPHONEUNIVERSAL_OUTPUT="${PRODUCT_DIR}/$CONFIGURATION-$ARCH_IPHONEUNIVERSAL"
+
 rm -rf "${BUILD_DIR}"
 mkdir "${BUILD_DIR}"
 mkdir "${PRODUCT_DIR}"
-mkdir "${PRODUCT_DIR}/iphoneos"
-mkdir "${PRODUCT_DIR}/iphonesimulator"
-mkdir "${PRODUCT_DIR}/iphoneuniversal"
+mkdir "$IPHONEOS_OUTPUT"
+mkdir "$IPHONEOSIMULATOR_OUTPUT"
+mkdir "$IPHONEUNIVERSAL_OUTPUT"
 
 xcodebuild  -project "${PROJECT_NAME}" \
             -target "${TARGET_NAME}" \
             -configuration "${CONFIGURATION}" \
-            -sdk "iphoneos" \
+            -sdk "$ARCH_IPHONEOS" \
             build \
             ONLY_ACTIVE_ARCH=NO \
+            IPHONEOS_DEPLOYMENT_TARGET=8.0 \
             BUILD_DIR="${BUILD_DIR}" \
-            TARGET_BUILD_DIR="${PRODUCT_DIR}/iphoneos"
+            TARGET_BUILD_DIR="${PRODUCT_DIR}/$CONFIGURATION-$ARCH_IPHONEOS"
 
 xcodebuild  -project "${PROJECT_NAME}" \
             -target "${TARGET_NAME}" \
             -configuration "${CONFIGURATION}" \
-            -sdk "iphonesimulator" \
+            -sdk "$ARCH_IPHONESIMULATOR" \
             build \
             ONLY_ACTIVE_ARCH=NO \
+            IPHONEOS_DEPLOYMENT_TARGET=8.0 \
             BUILD_DIR="${BUILD_DIR}" \
-            TARGET_BUILD_DIR="${PRODUCT_DIR}/iphonesimulator"
+            TARGET_BUILD_DIR="${PRODUCT_DIR}/$CONFIGURATION-$ARCH_IPHONESIMULATOR"
 
-# if [ -f "${PRODUCT_DIR}/iphoneos/${TARGET_NAME}.framework" ]; then
-    cp -r "${PRODUCT_DIR}/iphoneos/${TARGET_NAME}.framework" "${PRODUCT_DIR}"
+cp -r "$IPHONEOS_OUTPUT/${TARGET_NAME}.framework" "$IPHONEUNIVERSAL_OUTPUT"
+
+# if [ -f "${PRODUCT_DIR}/iphonesimulator/${TARGET_NAME}.framework/Modules/${TARGET_NAME}.swiftmodule/." ]; then
+#     cp -r "${PRODUCT_DIR}/iphonesimulator/${TARGET_NAME}.framework/Modules/${TARGET_NAME}.swiftmodule/." "${PRODUCT_DIR}/${TARGET_NAME}.framework/Modules/${TARGET_NAME}.swiftmodule/"
 # fi
 
-if [ -f "${PRODUCT_DIR}/iphonesimulator/${TARGET_NAME}.framework/Modules/${TARGET_NAME}.swiftmodule/." ]; then
-    cp -r "${PRODUCT_DIR}/iphonesimulator/${TARGET_NAME}.framework/Modules/${TARGET_NAME}.swiftmodule/." "${PRODUCT_DIR}/${TARGET_NAME}.framework/Modules/${TARGET_NAME}.swiftmodule/"
-fi
+lipo -create  "$IPHONEOS_OUTPUT/${TARGET_NAME}.framework/${TARGET_NAME}" \
+			  "$IPHONEOSIMULATOR_OUTPUT/${TARGET_NAME}.framework/${TARGET_NAME}" \
+	 -output "$IPHONEUNIVERSAL_OUTPUT/${TARGET_NAME}.framework/${TARGET_NAME}"
 
-lipo -create  "${PRODUCT_DIR}/iphoneos/${TARGET_NAME}.framework/${TARGET_NAME}" \
-			  "${PRODUCT_DIR}/iphonesimulator/${TARGET_NAME}.framework/${TARGET_NAME}" \
-			  -output "${PRODUCT_DIR}/${TARGET_NAME}.framework/${TARGET_NAME}"
+cp -r "$IPHONEUNIVERSAL_OUTPUT/${TARGET_NAME}.framework" "${PRODUCT_DIR}"
