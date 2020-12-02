df89960aa8050850ee72f1f4121993344c5c66f3
daydreamboy <wesley4chen@gmail.com>
2020-12-02 12:50:57 +0800

    add macro

--- WCMacroKit/WCMacroUncategoried.h
+++ WCMacroKit/WCMacroUncategoried.h
@@ -257,6 +257,34 @@ do { \
     } \
 } while (0)
 
+/**
+ Show alert with a text field
+ 
+ @param title
+ @param msg
+ @param _placeholder
+ @param cancel
+ @param dismissCompletion void (^dismissCompletion)(NSString *text)
+ - text the text of input
+ */
+#define SHOW_ALERT_WITH_INPUT(title, msg, _placeholder, cancel, dismissCompletion) \
+\
+do { \
+    if ([UIAlertController class]) { \
+        __block UITextField *localTextField; \
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:(title) message:(msg) preferredStyle:UIAlertControllerStyleAlert]; \
+        UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:(cancel) style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) { \
+            !dismissCompletion ?: dismissCompletion(localTextField.text); \
+        }]; \
+        [alert addAction:cancelAction]; \
+        [alert addTextFieldWithConfigurationHandler:^(UITextField *textField) { \
+            localTextField = textField; \
+            localTextField.placeholder = _placeholder; \
+        }]; \
+        [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:alert animated:YES completion:nil]; \
+    } \
+} while (0)
+
 #pragma mark - CoreFoundation
 
 #pragma mark > Toll-free Bridge
