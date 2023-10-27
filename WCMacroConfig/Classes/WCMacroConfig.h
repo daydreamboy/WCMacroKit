233364c343300bce58d0dc32ea87ac442fa378fd
daydreamboy <wesley4chen@gmail.com>
2023-10-27 10:00:26 +0800

    add code

--- WCMacroConfig/Classes/WCMacroConfig.h
+++ WCMacroConfig/Classes/WCMacroConfig.h
@@ -11,6 +11,14 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface WCMacroConfig : NSObject
 
+/**
+ Check code if compiled by Xcode13 or later
+ */
++ (BOOL)isXcode13OrLatterCompiled;
++ (BOOL)isXcode14OrLatterCompiled;
++ (BOOL)isXcode15OrLatterCompiled;
++ (BOOL)isDefinedDEBUG;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- WCMacroConfig/Classes/WCMacroConfig.m
+++ WCMacroConfig/Classes/WCMacroConfig.m
@@ -9,4 +9,36 @@
 
 @implementation WCMacroConfig
 
++ (BOOL)isXcode13OrLatterCompiled {
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 150000
+    return YES;
+#else
+    return NO;
+#endif
+}
+
++ (BOOL)isXcode14OrLatterCompiled {
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 160000
+    return YES;
+#else
+    return NO;
+#endif
+}
+
++ (BOOL)isXcode15OrLatterCompiled {
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 170000
+    return YES;
+#else
+    return NO;
+#endif
+}
+
++ (BOOL)isDefinedDEBUG {
+#if DEBUG
+    return YES;
+#else
+    return NO;
+#endif
+}
+
 @end
