947f2afe74dca43a4d6ed42fb8a82e8d413175f0
daydreamboy <wesley4chen@gmail.com>
2026-05-11 11:15:39 +0800

    add macro

--- WCMacroKit/WCMacroVersion.h
+++ WCMacroKit/WCMacroVersion.h
@@ -107,6 +107,11 @@
 #define IOS18_OR_LATER          ([[[UIDevice currentDevice] systemVersion] compare:@"18.0" options:NSNumericSearch] != NSOrderedAscending)
 #endif
 
+// >= `26.0`
+#ifndef IOS26_OR_LATER
+#define IOS26_OR_LATER          ([[[UIDevice currentDevice] systemVersion] compare:@"26.0" options:NSNumericSearch] != NSOrderedAscending)
+#endif
+
 // >= `X`, e.g. >= `15.4`
 /**
  X expected as a number, e.g. 13, 15.4, ...
